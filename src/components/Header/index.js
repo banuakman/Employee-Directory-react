2a4f3752cc5a48980df7b70ed7a0128a08a844b8
banuakman <banu@asterya.com>
2021-03-14 22:48:32 -0400

    create wrapper & header

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+
+function Title() {
+  return (
+    <div className="title">
+      <h1>Employee Directory</h1>
+      <p>
+        use the search box to narrow your results or click on carrots to filter
+        by heading
+      </p>
+    </div>
+  );
+}
+
+export default Title;
